b5bc14b1fe661e5c01a123b52ed848fdee11ff57
megakuo <megankuo@asu.edu>
2024-10-31 21:59:09 +0000

    ast string support

---
+++ mlir/test/Examples/DspExample/dsp_string.py
@@ -0,0 +1,9 @@
+def main() {
+        var a = [[[10,20],[30,0]] ];
+        var b = [[[40,50],[60,70]] ];
+        var c = sub(a, b);
+        print(c);
+
+        var d = "12345";
+        print(d);
+}
